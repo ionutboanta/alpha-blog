3b3a663838f5345734c54a88aefd0300072b408b
Ionut Boanta <ionut.boanta@crisser.ro>
2017-02-02 14:14:46 +0000

    Add partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,13 @@ class ArticlesController < ApplicationController
     
   end
   
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice]="Article was deleted"
+    redirect_to articles_path
+  end
+  
   def edit
     
     @article = Article.find(params[:id])
