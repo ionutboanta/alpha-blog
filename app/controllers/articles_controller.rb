3f851cb74a72ffca83e83b5585f66c76f39cedaf
Ionut Boanta <ionut.boanta@crisser.ro>
2017-02-03 13:08:46 +0000

    style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   def create
     @article=Article.new(article_params)
     if @article.save
-      flash[:notice]="Article was successfully created"
+      flash[:succsess]="Article was successfully created"
       redirect_to article_path(@article)
     else
       render :new
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice]="Article was deleted"
+    flash[:danger]="Article was deleted"
     redirect_to articles_path
   end
   
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice]="Article updated"
+      flash[:success]="Article updated"
       redirect_to article_path(@article)
     else
       render 'edit'
